efab826543bfa7d076ff8a0b3e6a6c53b77e8a51
Maya Harvey <mah992@nau.edu>
2026-03-18 10:11:16 -0700

    Added camera orbit, tweaked camera values

--- Assets/Scripts/LookAtAttractor.cs
+++ Assets/Scripts/LookAtAttractor.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 
 public class LookAtAttractor : MonoBehaviour
 {
+    // offset
     public Vector3 offset = new Vector3(30, 50, 30);
     // orbit
     public float orbitSpeed = 5f;
 
     void Update()
     {
-        // rotate the offset around the Y axis
+        // rotate the offset around the Y axis to orbit
         offset = Quaternion.Euler(0, orbitSpeed * Time.deltaTime, 0) * offset;
         
         transform.position = Attractor.POS + offset;
